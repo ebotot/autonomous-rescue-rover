0dc88c455a9887e7966f905cd01f1fc60da92236
JohnnyTranAC <74635740+JohnnyTranAC@users.noreply.github.com>
2020-11-17 18:38:50 -0800

    Update Interface.py

--- interface/Interface.py
+++ interface/Interface.py
@@ -36,21 +36,21 @@ camera = PiCamera()
 
 try:
 	while count < blinkCount:
-		GPIO.output(LEDPinY, True)							          # Yellow LED on acts like sensor waiting to detect
-		print("Asking prompt")								            # Prompt status asked in terminal
+		GPIO.output(LEDPinY, True)				# Yellow LED on acts like sensor waiting to detect
+		print("Asking prompt")					# Prompt status asked in terminal
 		display.lcd_display_string("Do you require", 1)		# First line:	14 characters
 		display.lcd_display_string("a park ranger?", 2)		# Second line:	14 characters
-		buttonPressG = GPIO.input(buttonPinG)				      # Detect button press input
+		buttonPressG = GPIO.input(buttonPinG)			# Detect button press input
 		buttonPressR = GPIO.input(buttonPinR)
 		if buttonPressG == False and ledStateG == False:	# Green(yes) button is pushed
 			GPIO.output(LEDPinY, False)
 			GPIO.output(LEDPinG, True)
 			ledStateG = True
-			print("Yes; Calling ranger")					          # Yes in terminal; 2 text lines on LCD
+			print("Yes; Calling ranger")			# Yes in terminal; 2 text lines on LCD
 			display.lcd_display_string("[Yes]   Calling", 1)# First line:	16 characters
-			display.lcd_display_string("for a ranger", 2)	  # Second line:	12 characters
+			display.lcd_display_string("for a ranger", 2)	# Second line:	12 characters
 			###camera.start_preview()
-			###camera.start_recording('home/pi/Destop/video.h264')		# Pi Camera recording video for 5 seconds
+			###camera.start_recording('home/pi/Destop/video.h264')	# Pi Camera recording video for 5 seconds
 			###sleep(5)
 			###camera.stop_recording()
 			###camera.stop_preview()
@@ -61,13 +61,13 @@ try:
 			GPIO.output(LEDPinY, False)
 			GPIO.output(LEDPinR, True)
 			ledStateR = True
-			print("No; Leave")								              # No in terminal; 2 text lines on LCD
-			display.lcd_display_string("[No]", 1)			      # First line:	4 characters
+			print("No; Leave")				# No in terminal; 2 text lines on LCD
+			display.lcd_display_string("[No]", 1)		# First line:	4 characters
 			display.lcd_display_string("Enjoy your hike", 2)# Second line:	15 characters
 			sleep(4)
 			GPIO.output(LEDPinR, False)
 			ledStateR = False
-		sleep(10)											                    # Wait 10 seconds before asking question
+		sleep(10)						# Wait 10 seconds before asking question
 
 finally:
 	### Reset the GPIO Pins to a safe (off) state and clears LCD of text
