fff8473e3a868d5c1785f7c01e62ca4bf6ee1048
JohnnyTranAC <74635740+JohnnyTranAC@users.noreply.github.com>
2021-01-07 16:54:24 -0800

    Update Interface.py
    Updated WIP btn response, audio added

--- interface/Interface.py
+++ interface/Interface.py
@@ -1,32 +1,14 @@
 import RPi.GPIO as GPIO
 import lcddriver
 from picamera import PiCamera
-from time import sleep
-
+import pygame
+import time
+import threading
 GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False) # Disable warnings
 
-blinkCount = 3
-count = 0
-
-### LED for yellow, green and red. Buttons for green (yes) and red (no)
-LEDPinY = 26
-LEDPinG = 22
-LEDPinR = 20
-buttonPinG = 5
-buttonPinR = 21
-
-### Setup the button
-GPIO.setup([buttonPinG, buttonPinR], GPIO.IN, pull_up_down = GPIO.PUD_UP)
-
-### Setup the pin the LED is connected to
-GPIO.setup([LEDPinY, LEDPinG, LEDPinR], GPIO.OUT)
-
-### Code internally keeps track of LED and button status
-buttonPressG = True
-buttonPressR = True
-ledStateY = False
-ledStateG = False
-ledStateR = False
+### Load pygame to play audio
+pygame.init()
 
 ### Load LCD driver to display
 display = lcddriver.lcd()
@@ -34,42 +16,90 @@ display = lcddriver.lcd()
 ### Load Pi Camera to capture
 camera = PiCamera()
 
-try:
-	while count < blinkCount:
-		GPIO.output(LEDPinY, True)				# Yellow LED on acts like sensor waiting to detect
-		print("Asking prompt")					# Prompt status asked in terminal
-		display.lcd_display_string("Do you require", 1)		# First line:	14 characters
-		display.lcd_display_string("a park ranger?", 2)		# Second line:	14 characters
-		buttonPressG = GPIO.input(buttonPinG)			# Detect button press input
-		buttonPressR = GPIO.input(buttonPinR)
-		if buttonPressG == False and ledStateG == False:	# Green(yes) button is pushed
-			GPIO.output(LEDPinY, False)
-			GPIO.output(LEDPinG, True)
-			ledStateG = True
-			print("Yes; Calling ranger")			# Yes in terminal; 2 text lines on LCD
-			display.lcd_display_string("[Yes]   Calling", 1)# First line:	16 characters
-			display.lcd_display_string("for a ranger", 2)	# Second line:	12 characters
-			###camera.start_preview()
-			###camera.start_recording('home/pi/Destop/video.h264')	# Pi Camera recording video for 5 seconds
-			###sleep(5)
-			###camera.stop_recording()
-			###camera.stop_preview()
-			sleep(4)
-			GPIO.output(LEDPinG, False)
-			ledStateG = False
-		elif buttonPressR == False and ledStateR == True:	# Red (no) button is pushed
-			GPIO.output(LEDPinY, False)
-			GPIO.output(LEDPinR, True)
-			ledStateR = True
-			print("No; Leave")				# No in terminal; 2 text lines on LCD
-			display.lcd_display_string("[No]", 1)		# First line:	4 characters
-			display.lcd_display_string("Enjoy your hike", 2)# Second line:	15 characters
-			sleep(4)
-			GPIO.output(LEDPinR, False)
-			ledStateR = False
-		sleep(10)						# Wait 10 seconds before asking question
+### Set I/O Pins for Ultrasonic Sensor
+TRIG = 23
+ECHO = 24
 
-finally:
-	### Reset the GPIO Pins to a safe (off) state and clears LCD of text
-	GPIO.cleanup()
-	display.lcd_clear()
+GPIO.setup(TRIG, GPIO.OUT)
+GPIO.setup(ECHO, GPIO.IN)
+
+### Setup GPIO input buttons and output LEDs
+BTN_G = 25              # Pin22, GPIO 25 - Green Button
+BTN_R = 18              # Pin12, GPIO 18 - Red Button
+
+LED_G = 5               # Pin29, GPIO 5 - Green LED
+LED_R = 6               # Pin31, GPIO 6 - Red LED
+
+BTN2LED = {
+    BTN_G: LED_G,
+    BTN_R: LED_R,}
+
+GPIO.setup([BTN_G, BTN_R], GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+GPIO.setup([LED_G, LED_R], GPIO.OUT, initial = GPIO.LOW)
+
+try:
+    while True:
+        
+        
+        GPIO.output(TRIG, False)
+        print "Waiting For Sensor To Settle"
+        
+        time.sleep(2)
+        
+        GPIO.output(TRIG, True)
+        time.sleep(0.00001)
+        GPIO.output(TRIG, False)
+        
+        while GPIO.input(ECHO) == 0:
+            pulse_start = time.time()
+        while GPIO.input(ECHO) == 1:
+            pulse_end = time.time()
+        
+        pulse_duration = pulse_end - pulse_start
+        
+        distance = pulse_duration * 17150
+        distance = round(distance, 2)
+        
+        display.lcd_clear()
+        display.lcd_display_string("Distance :", 1)
+        result = str(distance)+" cm"
+        display.lcd_display_string(result, 2);
+        
+        if distance < 20:
+            display.lcd_clear()
+            print("Asking prompt")                              # Prompt status asked in terminal
+            display.lcd_display_string("Do you require", 1)     # First line:   14 characters
+            display.lcd_display_string("a park ranger?", 2)     # Second line:  14 characters
+            pygame.mixer.music.load("1_Prompt_Female.wav")
+            pygame.mixer.music.play()
+            time.sleep(2)
+	
+            if GPIO.input(BTN_G):
+                GPIO.output(LED_G, True)
+                print("Yes; Calling ranger")                        # Yes in terminal; 2 text lines on LCD
+                display.lcd_clear()
+                display.lcd_display_string("[Yes]   Calling", 1)    # First line:   16 characters
+                display.lcd_display_string("for a ranger", 2)       # Second line:  12 characters
+		pygame.mixer.music.load("2_AnswerYes_Female.wav")
+		pygame.mixer.music.play()
+                camera.start_preview()                              # Start camera view
+                time.sleep(5)
+                camera.stop_preview()
+            elif GPIO.input(BTN_R):
+                GPIO.output(LED_R, True)
+                print("No; Leave")                              # No in terminal; 2 text lines on LCD
+                display.lcd_clear()
+                display.lcd_display_string("[No]", 1)           # First line:   4 characters
+                display.lcd_display_string("Enjoy your hike", 2)# Second line:  15 characters
+		pygame.mixer.music.load("2_AnswerNo_Female.wav")
+		pygame.mixer.music.play()
+
+        time.sleep(3)
+        GPIO.output([LED_G, LED_R], True)
+        
+        
+except KeyboardInterrupt: # If there is a KeyboardInterrupt (when you press Ctrl+C)
+    print("Cleaning up!") # exit the program and cleanup
+    GPIO.output([LED_G, LED_R], False)
+    GPIO.cleanup()
+    display.lcd_clear()
