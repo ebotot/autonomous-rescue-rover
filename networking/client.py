1cb417cd18475d6bf40dbc5f8d8bc52a6bb5310c
Tobe To <ebotot@gmail.com>
2021-02-18 16:15:55 -0800

    New code for sending GPS data

--- networking/client.py
+++ networking/client.py
@@ -3,7 +3,7 @@
 # Written with Python 3.9.0
 
 import socket
-
+import gps
 
 # IP and port the program will be connecting to
 # TODO: instead of hard-coding let user select
@@ -40,11 +40,27 @@ def write(client: 'file object', message: str):
 if __name__ == '__main__':
     client = connect()
 
-    # TODO: maybe take user input instead of predefined message, could be useful later
-    print("Sending message:")
-    write(client, "SAMPLE MESSAGE")
-    print("Sending exit message:")
-    write(client, "exit")
+    # Listen on port 2947 (gpsd) of localhost
+    session = gps.gps("localhost", "2947")
+    session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
+
+    while True:
+        try:
+            report = session.next()
+            # Wait for a 'TPV' report and display the current time
+            # To see all report data, uncomment the line below
+            # print(report)
+            if report['class'] == 'TPV':
+                if hasattr(report, 'time'):
+                    write(client, report.time)
+                    print(report.time)
+        except KeyError:
+            pass
+        except KeyboardInterrupt:
+            quit()
+        except StopIteration:
+            session = None
+            print("GPS collection terminated")
         
     disconnect(connection)
     
